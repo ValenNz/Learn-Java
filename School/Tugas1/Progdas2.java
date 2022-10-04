ac7cd9ad02fd550694d459de0f0eae2269028265
ValenNz <92833376+ValenNz@users.noreply.github.com>
2022-10-04 10:15:28 +0700

    Add files via upload

---
+++ School/Tugas1/Progdas2.java
@@ -0,0 +1,10 @@
+package Tugas1;
+
+public class Progdas2 {
+    public static void main(String[] args) {
+//        print : untuk baris tetap
+//        println : membuat baris baru
+        System.out.print("Belajar Pemrograman Dasar");
+        System.out.println("Belajar Pemrograman dasar");
+    }
+}
